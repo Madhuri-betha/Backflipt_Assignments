56420a1b74a3f8ac64efc79832024f2c6a1b5dca
Madhuri-betha <124079909+Madhuri-betha@users.noreply.github.com>
2023-02-02 12:04:58 +0530

    passing arguments in non interactive mode

---
+++ Assignment-31stJan2023/argument_passing.sh
@@ -0,0 +1,9 @@
+#counting num of arguments
+echo "Num of arguments $#" 
+if [ $# -gt 0 ]
+then
+  echo "$*\t" #printing aruguments
+  echo "sum of arguments $(expr $1 + $2 + $3 ) " 
+else
+  echo "no arguments"
+fi
